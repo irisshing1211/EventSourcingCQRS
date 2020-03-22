af6102de4e7b7fa4fd4a736f833ad64785c4f1ac
Iris Shing <irisshing1211@gmail.com>
2020-03-22 19:39:24 +0800

    add query

---
+++ Query/IQueryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventSourcingCQRS.Query
+{
+    public interface IQueryService<T>
+    {
+        Task<List<T>> GetAll();
+        T GetById(string id);
+    }
+}

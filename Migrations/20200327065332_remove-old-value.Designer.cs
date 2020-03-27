d2b9e031b1997625c19d42a1490514f5a43c9108
Iris Shing <irisshing1211@gmail.com>
2020-03-27 15:00:42 +0800

    update db

--- Migrations/20200327065332_remove-old-value.Designer.cs
+++ Migrations/20200327065332_remove-old-value.Designer.cs
@@ -4,14 +4,16 @@ using EventSourcingCQRS.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EventSourcingCQRS.Migrations
 {
     [DbContext(typeof(EventSourceContext))]
-    partial class EventSourceContextModelSnapshot : ModelSnapshot
+    [Migration("20200327065332_remove-old-value")]
+    partial class removeoldvalue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200327065332_remove-old-value.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventSourcingCQRS.Migrations
+{
+    public partial class removeoldvalue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OldValue",
+                table: "EventLogs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OldValue",
+                table: "EventLogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
